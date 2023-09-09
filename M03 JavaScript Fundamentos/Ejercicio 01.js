f546ad490c06e8cd4ab0dc07f6addd86cc5475ea
ydorisduran <duran241990@gmail.com>
2023-09-09 12:25:40 +0200

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,10 +5,10 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null;
+const nuevoString = "null";
 
 // Crea una variable de tipo number.
-const nuevoNumero = null;
+const nuevoNumero = 0;
 
 // Crea una variable de tipo boolean.
 const nuevoBoolean = null;
