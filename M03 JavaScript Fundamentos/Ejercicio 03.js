f6abeed3cf7fbfdc30310fd70b45594516cd735a
ydorisduran <duran241990@gmail.com>
2023-09-21 09:58:17 +0200

    ej 04

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,37 +5,91 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if(x === y) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = sonIguales(5, 5);
+console.log(resultado); // Esto imprimirá "true" en la consola
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if(str1.length === str2.length) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+ // Ejemplo de uso:(GPT)
+ var resultado = tienenMismaLongitud("hola", "adios");
+ console.log(resultado); // Esto imprimirá "true" en la consola
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+// ejemplo:(GPT)
+var resultado = menosQueNoventa(85);
+console.log(resultado); // el resultado sera "true" 
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num > 50) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = mayorQueCincuenta(60);
+console.log(resultado); // Esto imprimirá "true" en la consola
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 === 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = esPar(4);
+console.log(resultado); // Esto imprimirá "true" en la consola
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 !== 0) {
+      return true;
+   } else {
+      return false;
+   }
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = esImpar(5);
+console.log(resultado); // Esto imprimirá "true" en la consola
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,58 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   //var resultado = num * num;(GPT)
+   //return resultado;(GPT)
+   // return num ** 2;
+   return Math.pow(num, 2)
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = elevarAlCuadrado(4);
+console.log(resultado); // Esto imprimirá "16" en la consola
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   // var resultado = num * num * num;
+   //return resultado;
+   return num ** 3;  
 }
+
+// Ejemplo de uso:(GPT)
+var resultado = elevarAlCubo(3);
+console.log(resultado); // Esto imprimirá "27" en la consola
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   //var resultado = num ** exponent;
+   //return resultado; (GPT)
+   return Math.pow(num, exponent);
 }
+
+console.log(elevar(15,6))
+// Ejemplo de uso: (GPT)
+//var resultado = elevar(2, 3);
+//console.log(resultado); // Esto imprimirá "8" en la consola
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var redondeo = Math.round(num) // 0.49
+   return redondeo;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return Math.ceil(num);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random()
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
